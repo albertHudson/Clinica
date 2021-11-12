c24c6ba39af7754bc4dcaeef9dc5f2f6cd55af5e
Carlos <sos.carlos.hudson@gmail.com>
2021-11-11 21:35:34 -0300

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.template import loader
+
+from django.http import HttpResponse
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    nome = {
+        "Profissional":'Janaina Correa'
+    }
+    return HttpResponse(template.render(nome))
